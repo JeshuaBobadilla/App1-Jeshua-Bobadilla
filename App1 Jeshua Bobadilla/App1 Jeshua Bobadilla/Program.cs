33e0f460104ae4577474bc727f136ea91052f48e
JeshuaBobadilla <87060490+JeshuaBobadilla@users.noreply.github.com>
2021-08-09 17:30:57 -0600

    Add files via upload

---
+++ App1 Jeshua Bobadilla/App1 Jeshua Bobadilla/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+
+namespace App1_Jeshua_Bobadilla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string R = "s";
+            while (R == "s")
+            {
+                Console.WriteLine("Escribe el nombre del arhivo");
+                string archivo = Console.ReadLine();
+
+
+
+                StreamWriter ar;
+                ar = File.CreateText(archivo + ".txt");
+                ar.WriteLine("Archivo Creado");
+                ar.Close();
+
+
+
+                Console.WriteLine("Quieres crear otro archivo?");
+                Console.WriteLine("s/n");
+                R = Console.ReadLine();
+                Console.Clear();
+                if (R == "no")
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
+
+
